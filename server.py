dba2c462be80a645afd65956c3e643cbc8732f08
youya66 <nannbann@outlook.jp>
2016-08-04 23:19:42 +0900

    Add jwt

--- server.py
+++ server.py
@@ -5,16 +5,10 @@ from app.modules import twitter;
 import pymysql.cursors; 
 from app.env.secret import DATABASE_PASSWORD;
 
+import datetime;
+import time;
 import jwt;
-from Crypto.PublicKey import RSA
-from Crypto import Random
-
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives.asymmetric import rsa
-from cryptography.hazmat.primitives import serialization
-from cryptography.x509 import load_pem_x509_certificate
-
-import pprint
+import hmac;
 
 
 #database
@@ -39,47 +33,6 @@ app = SessionMiddleware(app(), session_opts);
 
 @route("/")
 def index():
-	private_key = rsa.generate_private_key(
-			public_exponent=65537,
-		    key_size=2048,
-		    backend=default_backend()
-		);
-
-	private_pem = private_key.private_bytes(
-		    encoding=serialization.Encoding.PEM,
-		   	format=serialization.PrivateFormat.TraditionalOpenSSL,
-		  	encryption_algorithm=serialization.NoEncryption()
-		);
-
-	public_key = private_key.public_key();
-	public_pem = public_key.public_bytes(
-			encoding=serialization.Encoding.PEM,
-			format=serialization.PublicFormat.SubjectPublicKeyInfo
-		);
-
-	print(public_pem);
-	key = load_pem_x509_certificate(private_key, default_backend())
-	print(key.public_key())
-	# random_func = Random.new().read;
-	# rsa = RSA.generate(1024,random_func);
-	# private_pem = rsa.exportKey(format="PEM",passphrase=None);
-
-	# with open("./app/env/private.pem","wb") as f:
-	# 	f.write(private_pem);
-
-	# public_pem = rsa.publickey().exportKey();
-	# with open("./app/env/public.pem","wb") as f:
-	# 	f.write(public_pem);
-
-	# private_key_str = open("./app/env/private.pem","rb").read();
-	# private_key = RSA.importKey(private_key_str).exportKey();
-	encoded = jwt.encode({'some': 'payload'}, private_pem, algorithm='HS256');
-
-	# public_key_str = open("./app/env/public.pem","rb").read();
-	# public_key = RSA.importKey(public_key_str).exportKey();
-
-	decoded = jwt.decode(encoded, key = private_pem, verify=True);
-	print(decoded)
 	return static_file("index.html",root="./static");
 
 @route("/static/<filename:path>")
@@ -94,7 +47,7 @@ def check_auth_state():
 def twitter_api():
 	auth_url, request_oauth_token, request_oauth_token_secret = twitter.generate_oauth_url();
 	data = {"request_oauth_token": request_oauth_token, "request_oauth_token_secret": request_oauth_token_secret};
-	
+
 	session = request.environ.get("beaker.session");
 	session["twitter"] = data;
 
@@ -137,19 +90,26 @@ def twitter_callback():
 		    	cursor.execute(sql,(twitter_user.id,twitter_user.screen_name,twitter_user.id))
 		    	connection.commit(twitter_user.id);
 		    	
-		    	jwt = generate_jwt();
+		    	jwt = generate_jwt(twitter_user.id);
     	else:
     		user_id = results["user_id"];
     		jwt = generate_jwt(user_id);
 
-    r = HTTPResponse(status=200, body={jwt: jwt},content_type="application/json; charset=utf8");
+
+    redirect("/?jwt="+ jwt);
+    
 
 def generate_jwt(user_id):
-	_rsa_public_key = open('./app/env/id_rsa', 'r').read();
-	rsa_public_key = RSA.importKey(_rsa_public_key);
-	key = rsa_public_key;
+	with open("./app/env/private.pem","rb") as f:
+		private_pem = f.read();
 
-	encoded = jwt.encode({"some": "payload"},key,algorithm='HS256');
+	exp = datetime.datetime.utcnow() + datetime.timedelta(days = 7);
+	secret = hmac.new(private_pem,None,"sha256").digest();
 
+	encoded = jwt.encode({"exp": exp,"user_id": user_id}, secret, algorithm="HS256").decode('utf-8');
+	return encoded;
+
+
+# decoded = jwt.decode(encoded, secret, verify=True);
 
 run(app=app,host="localhost",port="1234",debug=True, reloader=True);
